b3885b78041c8ff02761d554f52afb33ca576cb2
AnnaMuench <49953327+AnnaMuench@users.noreply.github.com>
2019-05-22 11:59:21 +0200

    Update specific analysis.R

--- specific analysis.R
+++ specific analysis.R
@@ -1,45 +1,29 @@
-#new dataframe with only erlotinib columns: 249 - 307
-erlotinib_treated <- NCI_TPW_gep_treated[, 249:307]
-erlotinib_untreated <- NCI_TPW_gep_untreated[, 249:307]
+#new dataframe with only erlotinib columns: 249 - 307 e=erlotinib
+#e_treated <- NCI_TPW_gep_treated[,which(colnames(NCI_TPW_gep_treated) == grep ("erlotinib", colnames(NCI_TPW_gep_treated))]
+e_treated <- NCI_TPW_gep_treated[, 249:307]
+e_untreated <- NCI_TPW_gep_untreated[, 249:307]
 
 #save as dataframe
-erlotinib_treated <- as.data.frame(erlotinib_treated)
-erlotinib_untreated <- as.data.frame(erlotinib_untreated)
+e_treated <- as.data.frame(e_treated)
+e_untreated <- as.data.frame(e_untreated)
 
-#combine data of treated and untreated in one 
-erlotinib_all <- cbind (erlotinib_treated, erlotinib_untreated)
-
-#Principal component analysis
-pca <- prcomp(erlotinib_all)
-plot(pca, type = "l")
-
-color <- ifelse(colnames(erlotinib_all) == colnames(erlotinib_treated), "forestgreen", "firebrick")
-plot(pca$rotation[,1], pca$rotation[,2], col = color)
-
-
-#Z-transormation
-median_gene <- apply (erlotinib_all, 1, function (x){
+library(GeneNet)
+z.transform(e_foldchange)
+#e_foldchange_z_transformed: treated-untreated + z-Transformation + PCA 
+e_foldchange <- e_treated - e_untreated
+median_gene <- apply (e_foldchange, 1, function (x){
   + median(x) })
-median(median_gene)
-sd_gene <- apply(erlotinib_all, 1, function (x){
+sd_gene <- apply(e_foldchange, 1, function (x){
   + sd(x) })
-sd (sd_gene)
-erlotinib_all_z_transformed <- (erlotinib_all-median(median_gene)) / sd(sd_gene)
+e_foldchange_z_transformed <- (e_folzdchange-median(median_gene)) / sd(sd_gene)
 
-#erlotinib_normalized_z_transformed: treated-untreated + z-Transformation + PCA 
-erlotinib_normalized <- erlotinib_treated - erlotinib_untreated
-median_gene <- apply (erlotinib_normalized, 1, function (x){
-  + median(x) })
-sd_gene <- apply(erlotinib_normalized, 1, function (x){
-  + sd(x) })
-erlotinib_normalized_z_transformed <- (erlotinib_normalized-median(median_gene)) / sd(sd_gene)
-
-pca <- prcomp(erlotinib_normalized_z_transformed)
-plot(pca$rotation[,1], pca$rotation[,2])
-
+pca <- prcomp(e_foldchange_z_transformed)
+plot(pca$rotation[,1], pca$rotation[,2], main = "z-transformed")
+pca <- prcomp(e_foldchange)
+plot(pca$rotation[,1], pca$rotation[,2], main = "foldchange")
 
 #select cell lines with highest variance
-var_cell_line <- apply(erlotinib_normalized_z_transformed, 2, function (x) {
+var_cell_line <- apply(e_foldchange_z_transformed, 2, function (x) {
   + var(x)})
 cell_line_var_greater_75quantile <- var_cell_line [which (var_cell_line > quantile(var_cell_line,0.75))]
 cell_line_var_decreasing_top15 <- sort(cell_line_var_greater_75quantile, decreasing = TRUE)
@@ -56,7 +40,7 @@ fviz_eig(pca) #shows percentage explained by each PC
 fviz_pca_ind(pca, col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
 
 #PCA with transformed matrix (each dot represents a gene):
-transformed_erlotinib_normalized_z_transformed <- t(erlotinib_normalized_z_transformed)
+transformed_e_foldchange_z_transformed <- t(e_foldchange_z_transformed)
 pca <- prcomp(transformed_erlotinib_normalized_z_transformed)
 plot(pca$rotation[,1], pca$rotation[,2])
 text(pca$rotation, labels = rownames(erlotinib_normalized_z_transformed), cex = 0.4, pos = 3)
