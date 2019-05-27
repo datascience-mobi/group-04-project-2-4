07dc45d29f4d8d491872c8c96f377a021c0b55f6
ann-sophiek <49977570+ann-sophiek@users.noreply.github.com>
2019-05-27 23:02:48 +0200

    Update specific analysis.R

--- specific analysis.R
+++ specific analysis.R
@@ -1,59 +1,60 @@
-#new dataframe with only erlotinib columns: 249 - 307 e=erlotinib
-#e_treated <- NCI_TPW_gep_treated[,which(colnames(NCI_TPW_gep_treated) == grep ("erlotinib", colnames(NCI_TPW_gep_treated))]
-e_treated <- NCI_TPW_gep_treated[, 249:307]
-e_untreated <- NCI_TPW_gep_untreated[, 249:307]
+#new matrix only with samples/columns treated with erlotinib  (e=erlotinib)
+e_treated <- NCI_TPW_gep_treated[,grep ("erlotinib", colnames(NCI_TPW_gep_treated))]
+e_untreated <- NCI_TPW_gep_untreated[,grep ("erlotinib", colnames(NCI_TPW_gep_untreated))]
+e_foldchange <- e_treated - e_untreated
 
-#save as dataframe
-e_treated <- as.data.frame(e_treated)
-e_untreated <- as.data.frame(e_untreated)
-
+#e_foldchange_z_transformed: z-Transformation 
 library(GeneNet)
 z.transform(e_foldchange)
-#e_foldchange_z_transformed: treated-untreated + z-Transformation + PCA 
-e_foldchange <- e_treated - e_untreated
 median_gene <- apply (e_foldchange, 1, function (x){
   + median(x) })
 sd_gene <- apply(e_foldchange, 1, function (x){
   + sd(x) })
-e_foldchange_z_transformed <- (e_folzdchange-median(median_gene)) / sd(sd_gene)
+e_foldchange_z_transformed <- (e_foldchange-median(median_gene)) / sd(sd_gene)
 
-pca <- prcomp(e_foldchange_z_transformed)
-plot(pca$rotation[,1], pca$rotation[,2], main = "z-transformed")
-pca <- prcomp(e_foldchange)
-plot(pca$rotation[,1], pca$rotation[,2], main = "foldchange")
 
-#select cell lines with highest variance
+# table of 15 cell lines with highest variance to show most regulated cell lines
+#select 15 cell lines with highest variance (greater than 75% quantile, sorted by decreasing value)
 var_cell_line <- apply(e_foldchange_z_transformed, 2, function (x) {
   + var(x)})
-cell_line_var_greater_75quantile <- var_cell_line [which (var_cell_line > quantile(var_cell_line,0.75))]
-cell_line_var_decreasing_top15 <- sort(cell_line_var_greater_75quantile, decreasing = TRUE)
+cell_line_var_greater_75quantile <- sort(var_cell_line [which (var_cell_line > quantile(var_cell_line,0.75))], decreasing = TRUE)
+rm(var_cell_line)
 
-table_cell_lines_var_top15 <- cbind(names(cell_line_var_decreasing_top15), cell_line_var_decreasing_top15)
-rownames(table_cell_lines_var_top15) <- c(1:15)
+#create a table containing the name and the variance of the 15 cell lines with highest variance
+table_cell_lines_var_top15 <- cbind(names(cell_line_var_greater_75quantile), cell_line_var_greater_75quantile)
+rownames(table_cell_lines_var_top15) <- c(1:nrow(table_cell_lines_var_top15))
 colnames(table_cell_lines_var_top15) <- c("cell line", "variance")
 #kable(): simple table formatting function
 kable(table_cell_lines_var_top15)
 
 
-#PCA visualization with factoextra package: 
-fviz_eig(pca) #shows percentage explained by each PC
-fviz_pca_ind(pca, col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
-
-#PCA with transformed matrix (each dot represents a gene):
-transformed_e_foldchange_z_transformed <- t(e_foldchange_z_transformed)
-pca <- prcomp(transformed_erlotinib_normalized_z_transformed)
+#PCA to show most regulated cell lines
+#PCA with transformed matrix (each point represents a sample):
+e_foldchange_matrix_transformed <- t(e_foldchange_z_transformed)
+pca <- prcomp(e_foldchange_matrix_transformed)
 plot(pca$rotation[,1], pca$rotation[,2])
-text(pca$rotation, labels = rownames(erlotinib_normalized_z_transformed), cex = 0.4, pos = 3)
+text(pca$rotation, labels = rownames(e_foldchange_z_transformed), cex = 0.4, pos = 3)
 
 #PCA visualization with factoextra package (according to www.sthda.com/english/articles/31-principal-component-methods-in-r-practical-guide/118-principal-component-analysis-in-t-prcomp-vs-princomp/) : 
-fviz_eig(pca) #shows percentage explained by each PC
+#plot of cell lines with gradient colors by their quality of representation
+fviz_pca_ind(pca, col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07")) #cos2 values correspond to their quality of representation
 
-#plot of cell lines with gradient colors by the quality of representation
-fviz_pca_ind(pca, col.ind = "cos2", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
 
-#plot of genes showing their contribution
+#bar plot showing the percentage explained by each PC
+'fviz_eig(pca) 
+#plot of genes showing their contribution to the two PCs 
 fviz_pca_var(pca, col.var ="contrib", gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"))
-
 #get contribution of genes to the PCs
 results.genes <- get_pca_var(pca)
-genes_pca_highest_contribution <- results.genes$contrib
+genes_pca_highest_contribution <- results.genes$contrib '
+
+
+#mean of gene expression of each gene over all cell lines
+e_treated_mean_over_cell_lines <- rowMeans(e_treated)
+e_untreated_mean_over_cell_lines <- rowMeans(e_untreated)
+e_foldchange_mean_over_cell_lines <- rowMeans(e_foldchange) #equal to e_treated_mean_over_cell_lines - e_untreated_mean_over_cell_line
+
+#determine the p-value for a apired two-sample t-test 
+p_values <- sapply(1:length(e_treated_mean_over_cell_lines), function(x) {
+  +   t.test(e_treated_mean_over_cell_lines[x], e_untreated_mean_over_cell_lines[x])$p.value})
+FDR(pvals, qlevel = 0.05)
