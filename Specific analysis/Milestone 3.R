9b2fb8505efab9d11215b2890f4527fea4cf83dc
AnnaMuench <49953327+AnnaMuench@users.noreply.github.com>
2019-06-21 00:04:54 +0200

    Update Milestone 3.R

--- Specific analysis/Milestone 3.R
+++ Specific analysis/Milestone 3.R
@@ -48,5 +48,63 @@ res
 
 lm()
 
+
+
+
+# Expressionsdaten von EGFR Expressione relativ to controle 
+#--> Einfluss der therapie auf die Erpression des Her 1 genes
+
+plot(NegLogGI50_59_celllines_neg, e_foldchange_normalized ["EGFR",], 
+      col = e_color_cancertype, 
+      pch = 19, 
+      xlab = "logGI50", 
+      ylab = "EGFR Expression (log2, relative to control)",        
+      main = "Erlotinib 24h")
+
+legend(x = -3.8, y = 2, 
+      legend = names(color_palette_cancertype), 
+      col = color_palette_cancertype, 
+      pch = 19)
+
+#label only points which have a dicreased Her 1 expression after erlotinib treatment
+labeled_celllines <- names(NegLogGI50_59_celllines_neg)[ e_foldchange_normalized["EGFR", ] < 0]
+
+text(NegLogGI50_59_celllines_neg[labeled_celllines], e_foldchange_normalized ["EGFR", labeled_celllines], 
+      labels = labeled_celllines,
+      cex = 0.7,
+      pos = 3) #position of text at the top of the point
+
+
+
+
+   
+# Her 1 gene expression in the untreated celllines gegen die GI50     
+plot(NegLogGI50_59_celllines_neg, e_untreated ["EGFR",], 
+      col = e_color_cancertype, 
+      pch = 19, 
+     xlab = "logGI50", 
+     ylab = "EGFR Expression (untreated)",        
+     main = "Expression of the epidermal growth factor receptor (Her 1)")
+   
+legend(x = -3.8, y = 11, 
+    legend = names(color_palette_cancertype), 
+    col = color_palette_cancertype, 
+    pch = 19)
+
+text(NegLogGI50_59_celllines_neg, e_untreated ["EGFR",], labels  = NegLogGI50_59_cellline_names,cex= 0.7, pos=3)   
+
+
+#ERROR      
+# lable celline which belong to cluster 3 in the heatmap in milestone 4
+labeled_cellline_clusters <- names(NegLogGI50_59_celllines_neg)[ cellline_clusters [,1]  > 2 ]
+text(NegLogGI50_59_celllines_neg[labeled_cellline_clusters], e_untreated[ "EGFR",],
+    labels = labeled_cellline_clusters,
+    cex = 0.7,
+    pos = 3)  
+   
+
+
+
+   
 'to do:
 linear regression'
